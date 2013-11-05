0f98c6e20c43963f51778e7cfd939230ccfeb29d
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-11-05 18:53:18 -0500

    Comment out debugging prints

--- Python/Serial2ArdFunction.py
+++ Python/Serial2ArdFunction.py
@@ -5,7 +5,7 @@ import time
 
 ##Functions
 
-def InitializeSerial(): ##Initialize the Pi<-->Arduino serial link. serial object called "slink"
+def InitializeSerial(): ##Initialize the Pi<-->Arduino serial link. serial 								object called "slink"
  	# Opening the serial port resets the Arduino. This waits for the Arduino to print 'Ready' to indicate that it has started and is prepared to send/recieve data
  	global slink  #Saves us from passing serial link into each function
 	ArduinoReady=2000	#Initialize variable used for confirming Arduino readiness
@@ -39,13 +39,12 @@ def Pi2Ard(rs,ls,ld,rd):	##Pi to Arduino serial communications
 	Transmission=RightSpeed+LeftSpeed+RightDirection+LeftDirection+chr(003)
 
 	slink.write(Transmission) #Write the new message to the serial port
-	print "Transmission : " + Transmission #Print confirmation of sent message
+	#print "Transmission : " + Transmission #Print confirmation of sent message
 
 def CloseSerial():  ##Close Pi<-->Arduino serial link
 	slink.write(str(00000000)+chr(003)) #release the motors when finished
 	slink.close() #After data collection, the serial port is closed.
-	print 'Done' #"Done" is printed to verify a successful ending of the program.
-
+	print 'Serial Link Closed' #'Serial Link Closed' is printed to verify a successful closing of the serial connection
 
 ##Code
 
