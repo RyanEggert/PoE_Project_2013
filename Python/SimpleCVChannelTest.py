50e87eb0fc75e32530175da5025ab19fc82a1bdd
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-11-10 23:09:14 -0500

    Create b & g threshold variables

--- Python/SimpleCVChannelTest.py
+++ Python/SimpleCVChannelTest.py
@@ -4,6 +4,8 @@ from SimpleCV import *
 import time
 from numpy import mean
 
+BlueThreshold=100
+GreenThreshold=100
 
 img=Image('C://Users//reggert//Documents//My Box Files//Olin//Year II//POE//Project//testimage.jpg')
 imgmat=img.getNumpy()
@@ -27,7 +29,7 @@ for n in range(len(pts)):
 	bv=bmat[rmvpx[n]][rmvpy[n]][2]
 	gv=gmat[rmvpx[n]][rmvpy[n]][1]
 
-	if bv<100 and gv<100:  #if not much blue and green, then it is red
+	if bv<BlueThreshold and gv<GreenThreshold:  #if not much blue and green, then it is red
 		redx.append(rmvpx[n])
 		redy.append(rmvpy[n])
 
@@ -36,16 +38,13 @@ redPTS=zip(redx,redy)
 (AvgX,AvgY)=tuple(map(mean, zip(*redPTS)))
 AvgMax=(int(AvgX), int(AvgY))
 
-print redPTS
-print AvgMax
 xtt,ytt=AvgMax
-print imgmat[xtt][ytt]
 
 red.drawPoints(redPTS, color=Color.BLUE, sz=1, width=-1)
 red.drawPoints([AvgMax], color=Color.BLACK,sz=3,width=-1)
 	
 d=red.show(type='window')
 
-time.sleep(5)
+time.sleep(15)
 
 d.quit()
